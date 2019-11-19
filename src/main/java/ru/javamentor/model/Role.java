e5601b91304867678627f7b8c55d507ffb09c091
nick <gnn@mm.st>
2019-11-19 22:15:24 +0300

    mark Role bean

--- src/main/java/ru/javamentor/model/Role.java
+++ src/main/java/ru/javamentor/model/Role.java
@@ -2,15 +2,61 @@ package ru.javamentor.model;
 
 import org.springframework.security.core.GrantedAuthority;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "roles")
 public class Role implements GrantedAuthority {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @ManyToMany(mappedBy = "roles")
     private List<User> user;
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<User> getUser() {
+        return user;
+    }
+
+    public void setUser(List<User> user) {
+        this.user = user;
+    }
 
     @Override
     public String getAuthority() {
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", user=" + user +
+                '}';
     }
 }
