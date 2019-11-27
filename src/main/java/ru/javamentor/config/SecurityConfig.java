c47f4419580de14bd4d46c0e87b4095385bda783
nick <gnn@mm.st>
2019-11-27 19:57:42 +0300

    remove Autowired from field

--- src/main/java/ru/javamentor/config/SecurityConfig.java
+++ src/main/java/ru/javamentor/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,8 +18,12 @@ import ru.javamentor.service.UserService;
 @EnableWebSecurity
 @ComponentScan("ru.javamentor")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
     private UserService userService;
+
+    @Autowired
+    public SecurityConfig(@Lazy UserService userService) {
+        this.userService = userService;
+    }
 
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
