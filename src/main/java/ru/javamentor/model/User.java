92dc70fe5899455e66b6c3af8d89162a9af90f1a
nick <gnn@mm.st>
2019-12-05 14:18:26 +0300

    modify User

--- src/main/java/ru/javamentor/model/User.java
+++ src/main/java/ru/javamentor/model/User.java
@@ -38,7 +38,6 @@ public class User implements UserDetails {
                     name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(
                     name = "role_id", referencedColumnName = "id"))
-
     private Set<Role> roles;
 
     public User() {}
